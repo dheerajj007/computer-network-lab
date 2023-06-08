12d2bb573a62c3538e1a681c9c9ee545cf88febc
fixxtion <dheerajkumarr235@gmail.com>
2023-06-09 00:06:17 +0530

    updates

--- Wireshark/script.js
+++ Wireshark/script.js
@@ -8,30 +8,36 @@ window.onload = function () {
 
     // Iterate over each account input and capture the values
     for (let i = 0; i < accountInputs.length; i++) {
-      const accountName = accountInputs[i].querySelector(
-        'input[name="accountName"]'
-      ).value;
-      const accountNumber = accountInputs[i].querySelector(
-        'input[name="accountNumber"]'
-      ).value;
       const accountBranch = accountInputs[i].querySelector(
         'input[name="accountBranch"]'
       ).value;
+      const accountID = accountInputs[i].querySelector(
+        'input[name="accountID"]'
+      ).value;
+      const cycleDetails = {
+        monthly: accountInputs[i].querySelector('input[name="monthly"]')
+          .checked,
+        daily: accountInputs[i].querySelector('input[name="daily"]').checked,
+        weekly: accountInputs[i].querySelector('input[name="weekly"]').checked,
+      };
+      const flag1 = accountInputs[i].querySelector(
+        'input[name="flag1"]'
+      ).checked;
+      const flag2 = accountInputs[i].querySelector(
+        'input[name="flag2"]'
+      ).checked;
 
       accounts.push({
-        accountName: accountName,
-        accountNumber: accountNumber,
         accountBranch: accountBranch,
+        accountID: accountID,
+        cycleDetails: cycleDetails,
+        flag1: flag1,
+        flag2: flag2,
       });
     }
 
-    const cycleDetails = document.getElementById("cycleDetails").value;
-
     // Generate the feature file content based on the inputs
-    const featureFileContent = generateFeatureFileContent(
-      accounts,
-      cycleDetails
-    );
+    const featureFileContent = generateFeatureFileContent(accounts);
 
     // Download the generated feature file
     downloadFeatureFile(featureFileContent);
@@ -48,55 +54,67 @@ window.onload = function () {
 
     accountInput.innerHTML = `
             <h3>Account Details ${accountCount}</h3>
-            <label for="accountName">Account Name:</label>
-            <input type="text" name="accountName" required><br><br>
-
-            <label for="accountNumber">Account Number:</label>
-            <input type="text" name="accountNumber" required><br><br>
-
             <label for="accountBranch">Account Branch:</label>
             <input type="text" name="accountBranch" required><br><br>
 
-            <button type="button" class="removeAccountButton">Remove Account</button><br><br>
+            <label for="accountID">Account ID:</label>
+            <input type="text" name="accountID" required><br><br>
+
+            <label for="cycleDetails">Cycle Details:</label>
+            <input type="checkbox" name="monthly"> Monthly
+            <input type="checkbox" name="daily"> Daily
+            <input type="checkbox" name="weekly"> Weekly<br><br>
+
+            <label for="flag1">Flag 1:</label>
+            <input type="checkbox" name="flag1"><br><br>
+
+            <label for="flag2">Flag 2:</label>
+            <input type="checkbox" name="flag2"><br><br>
         `;
 
     accountInputs.appendChild(accountInput);
-
-    const removeAccountButton = accountInput.querySelector(
-      ".removeAccountButton"
-    );
-    removeAccountButton.addEventListener("click", function () {
-      accountInputs.removeChild(accountInput);
-    });
   });
 
-  function generateFeatureFileContent(accounts, cycleDetails) {
+  function generateFeatureFileContent(accounts) {
     let content = "";
 
     content += "Feature: Account Management\n\n";
+    content += `Scenario: Account Creation\n\n`;
 
     for (let i = 0; i < accounts.length; i++) {
       const account = accounts[i];
 
-      content += `  Scenario: Account Creation ${i + 1}\n`;
-
-      // Add multiple steps for each account
-      content += `    Given I have the following account details:\n`;
-      content += `      | Name   | ${account.accountName} |\n`;
-      content += `      | Number | ${account.accountNumber} |\n`;
-      content += `      | Branch | ${account.accountBranch} |\n`;
-
-      content += `    When I initiate the account creation\n`;
-      content += `    Then the account should be created successfully\n\n`;
+      content += `  Given I have the following account details:\n`;
+      content += `    | Branch | ${account.accountBranch} |\n`;
+      content += `    | ID     | ${account.accountID} |\n`;
+      content += `    | Cycle Details | ${getCycleDetails(
+        account.cycleDetails
+      )} |\n`;
+      content += `    | Flag 1 | ${account.flag1} |\n`;
+      content += `    | Flag 2 | ${account.flag2} |\n\n`;
+      content += `  When I initiate the account creation\n`;
+      content += `  Then the account should be created successfully\n\n`;
     }
 
-    content += `  Scenario: Cycle Details\n`;
-    content += `    Given I have the following cycle details:\n`;
-    content += `      | Details | ${cycleDetails} |\n`;
-    content += `    When I perform the cycle operation\n`;
-    content += `    Then the operation should be successful\n`;
-
     return content;
+  }
+
+  function getCycleDetails(cycleDetails) {
+    let details = "";
+
+    if (cycleDetails.monthly) {
+      details += "Monthly ";
+    }
+
+    if (cycleDetails.daily) {
+      details += "Daily ";
+    }
+
+    if (cycleDetails.weekly) {
+      details += "Weekly";
+    }
+
+    return details.trim();
   }
 
   function downloadFeatureFile(content) {
